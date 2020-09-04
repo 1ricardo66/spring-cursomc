ddfaceb5ea379886d50d68c706e9818ee48dc428
1ricardo66 <ricardofps66@gmail.com>
2020-09-04 10:54:44 -0300

    Criando operação de instanciação

--- src/main/java/com/ricardofelix/cursomc/CursomcApplication.java
+++ src/main/java/com/ricardofelix/cursomc/CursomcApplication.java
@@ -1,13 +1,41 @@
 package com.ricardofelix.cursomc;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursomcApplication {
+import com.ricardofelix.cursomc.models.Categoria;
+import com.ricardofelix.cursomc.repository.CategoriaRepository;
 
+@SpringBootApplication
+public class CursomcApplication implements CommandLineRunner{
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		List<Categoria> lista = new ArrayList<>();
+		
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+		
+		
 	}
 
 }
